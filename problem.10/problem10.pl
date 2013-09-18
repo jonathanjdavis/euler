0a1bb639ddf22b431df05e7ff83b57112c273dc8
Peter Blair <pblair@tucows.com>
2013-09-18 00:10:56 -0400

    first attempt

---
+++ problem.10/problem10.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+
+use lib '../lib';
+use Euler::Prime::Modulus 'is_prime';
+
+my $MAX = 2000000;
+
+my @primes = ();
+
+for( my $i = 2; $i < $MAX; $i++ ){
+	push( @primes, $i ) if is_prime( $i );	
+}
+
+my $sum = 0;
+$sum += $_ for @primes;
+
+print "$sum\n";
