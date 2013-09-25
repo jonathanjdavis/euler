12b0005adb308132aae21ba9024d544e19509930
Jonathan <davis@snickers.org>
2013-09-25 08:05:38 -0400

    Minor fix

--- problem.7/problem7.py
+++ problem.7/problem7.py
@@ -8,7 +8,7 @@ x = 13
 while pos != 10001 :
     x += 2
     prime = True
-    for n in range(2,round(math.sqrt(x))+1):
+    for n in range(2,round(math.sqrt(x))):
         if x%n == 0:
             prime = False
             break
